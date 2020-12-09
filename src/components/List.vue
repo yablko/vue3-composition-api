1814c654c15e87f5a9cdce2cf788a43296021222
yablko <yablko@brm.sk>
2020-12-09 18:24:49 +0100

    03-TRULO-ORIGINS

---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="list">
+    <header>
+      <h3 class="p-1 font-bold leading-none text-gray-900">{{ title }}</h3>
+    </header>
+
+    <ul>
+      <Card text="Lost Cities"></Card>
+      <Card text="Summoner Wars: Master Set"></Card>
+      <Card text="Star Realms"></Card>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+import Card from '@/components/Card.vue'
+
+export default {
+  components: {
+    Card
+  },
+  props: {
+    title: {
+      type: String
+    }
+  },
+  setup(props) {
+    console.log(props.title)
+
+    const state = reactive({
+      count: 0
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/css/app.css'
+import './assets/css/trulo.css'
 
 createApp(App)
   .use(router)
