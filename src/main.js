f42d53a3f47163f4d200029ccf34f2e2f34be79d
yablko <yablko@brm.sk>
2020-12-09 15:55:49 +0100

    00-COMPAPI-ORIGINS

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/css/app.css'
 
 createApp(App)
   .use(router)
-  .mount("#app");
+  .mount('#app')
