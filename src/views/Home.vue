c208195099a4e6b4e57c5f4877ec821df5dd01d7
yablko <yablko@brm.sk>
2020-12-09 16:29:09 +0100

    01-COMPAPI-REF-COMPUTED

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <button @click="score++">{{ score }} twice is {{ double }}</button>
+
+    <p>
+      {{ hello }}
+    </p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { ref, computed } from 'vue'
+
+export default {
+  setup() {
+    // data
+    const score = ref(69)
+    const dude = ref('Bulma')
+
+    // computed
+    const double = computed(() => score.value * 2)
+    const hello = computed(() => 'Hello, I am ' + dude.value)
+
+    return { score, double, hello }
+  }
+}
 </script>
